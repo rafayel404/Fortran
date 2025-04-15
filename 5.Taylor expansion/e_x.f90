2b121fd6fc80ca48a7bff59e0d6d9700204f1ba3
Rafayel Shium <smdrafayel@gmail.com>
2025-04-15 11:00:13 +0600

    Add files via upload

---
+++ 5.Taylor expansion/e_x.f90
@@ -0,0 +1,31 @@
+! Determine the third degree Taylor polynomial of e^x centered at x = 0 and approximate the value at x = 1
+
+program Taylor_polynomial_e_x
+
+    implicit none
+    real::x,sum,term
+    integer::n,i
+
+    write (*, *) "Enter x, Degree n:"
+    read (*, * ) x, n
+
+    sum = 0.0
+    term = 1.0
+
+    do i=0,n
+        sum=sum+((x**i)/factorial(i))
+    end do
+
+    write (*, *) "Taylor polynomial P(x) at x =", x, 'is', sum
+
+   contains
+        integer function factorial(n)
+        integer,intent(in)::n
+        integer::i
+        factorial=1
+        do i=1,n
+            factorial=factorial*i
+        end do
+    end function factorial
+
+end program Taylor_polynomial_e_x
---
+++ 5.Taylor expansion/sin_x.f90
@@ -0,0 +1,43 @@
+program Taylor_polynomial_sinx
+
+    implicit none
+    real::x,sum,k
+    integer::n,i,count=0,sign=1
+
+    write (*, *) "Enter x, Degree n:"
+    read (*, * ) x, n
+
+    sum = 0.0
+
+    do i=1,100
+
+        if ((mod(i,2)/=0))then
+            if(mod(sign,2)==0)then
+               k=((-1)**i)*x**i
+
+            else
+                k=((-1)**(i+1))*x**i
+            end if
+
+            sum=sum+(k/factorial(i))
+            count=count+1
+            if(count==n)exit
+            sign=sign+1
+
+        end if
+
+    end do
+
+    write (*, *) "Taylor polynomial Sin(x) at x =", x, 'is', sum
+
+   contains
+        integer function factorial(n)
+        integer,intent(in)::n
+        integer::i
+        factorial=1
+        do i=1,n
+            factorial=factorial*i
+        end do
+    end function factorial
+
+end program Taylor_polynomial_sinx
